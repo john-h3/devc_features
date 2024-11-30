8c98ab4ff46e6dea39948c2238d8334d4594400e
johnh3 <github@yahui.tech>
2024-11-30 15:45:12 +0800

    update node

--- src/node/install.sh
+++ src/node/install.sh
@@ -5,14 +5,29 @@ set -e
 NODE_MIRROR=${NODEMIRROR:-"nju"}
 NODE_VERSION=${NODEVERSION:-"lts"}
 NPM_MIRROR=${NPMMIRROR:-"tencent"}
+GH_PROXY=${GHPROXY:-""}
+
+nvm_install_shell_url=https://raw.githubusercontent.com/nvm-sh/nvm/refs/heads/master/install.sh
+
+# 判断 URL 是否以 / 结尾
+if [[ "$GH_PROXY" == */ ]]; then
+  # 去掉结尾的 /
+  GH_PROXY="${GH_PROXY%/}"
+fi
+if [ "${GH_PROXY}" != "" ]; then
+  nvm_install_shell_url=$GH_PROXY/$nvm_install_shell_url
+fi
 
 echo "Installing nvm..."
 
 mkdir -p $NVM_DIR
-curl -L https://gh.yahui.tech/https://raw.githubusercontent.com/nvm-sh/nvm/refs/heads/master/install.sh -o /tmp/install.sh
 
-sed -i 's#https://raw#https://gh.yahui.tech/https://raw#g' /tmp/install.sh
-sed -i 's#https://github#https://gh.yahui.tech/https://github#g' /tmp/install.sh
+curl -L $nvm_install_shell_url -o /tmp/install.sh
+
+if [ "${GH_PROXY}" != "" ]; then
+  sed -i "s#https://raw#${GH_PROXY}/https://raw#g" /tmp/install.sh
+  sed -i "s#https://github#${GH_PROXY}/https://github#g" /tmp/install.sh
+fi
 
 bash /tmp/install.sh
 
@@ -22,57 +37,57 @@ echo "Nvm installed."
 
 # 根据用户选项设置 node 镜像源
 case "${NODE_MIRROR}" in
-  "tsinghua")
-    export NVM_NODEJS_ORG_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/
-    ;;
-  "ustc")
-    export NVM_NODEJS_ORG_MIRROR=https://mirrors.ustc.edu.cn/node/
-    ;;
-  "aliyun")
-    export NVM_NODEJS_ORG_MIRROR=https://mirrors.aliyun.com/nodejs-release/
-    ;;
-  "nju")
-    export NVM_NODEJS_ORG_MIRROR=https://mirror.nju.edu.cn/nodejs-release/
-    ;;
-  *)
-    echo "Invalid mirror option: ${MIRROR}"
-    exit 1
-    ;;
+"tsinghua")
+  export NVM_NODEJS_ORG_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/
+  ;;
+"ustc")
+  export NVM_NODEJS_ORG_MIRROR=https://mirrors.ustc.edu.cn/node/
+  ;;
+"aliyun")
+  export NVM_NODEJS_ORG_MIRROR=https://mirrors.aliyun.com/nodejs-release/
+  ;;
+"nju")
+  export NVM_NODEJS_ORG_MIRROR=https://mirror.nju.edu.cn/nodejs-release/
+  ;;
+*)
+  echo "Invalid mirror option: ${MIRROR}"
+  exit 1
+  ;;
 esac
 
 # Adjust node version if required
 if [ "${NODE_VERSION}" = "none" ]; then
-    NODE_VERSION=
+  NODE_VERSION=
 elif [ "${NODE_VERSION}" = "lts" ]; then
-    NODE_VERSION="lts/*"
+  NODE_VERSION="lts/*"
 elif [ "${NODE_VERSION}" = "latest" ]; then
-    NODE_VERSION="node"
+  NODE_VERSION="node"
 fi
 
 nvm install $NODE_VERSION
 if [ "${NODE_VERSION}" != "" ]; then
-    nvm alias default "${NODE_VERSION}"
-    nvm use $NODE_VERSION
+  nvm alias default "${NODE_VERSION}"
+  nvm use $NODE_VERSION
 fi
 
 # 根据用户选项设置 npm 镜像源
 case "${NPM_MIRROR}" in
-  "tencent")
-    npm config set registry http://mirrors.tencent.com/npm/
-    ;;
-  "ustc")
-    npm config set registry https://npmreg.proxy.ustclug.org/
-    ;;
-  "aliyun")
-    npm config set registry https://registry.npmmirror.com
-    ;;
-  "nju")
-    npm config set registry https://repo.nju.edu.cn/repository/npm/
-    ;;
-  *)
-    echo "Invalid mirror option: ${MIRROR}"
-    exit 1
-    ;;
+"tencent")
+  npm config set registry http://mirrors.tencent.com/npm/
+  ;;
+"ustc")
+  npm config set registry https://npmreg.proxy.ustclug.org/
+  ;;
+"aliyun")
+  npm config set registry https://registry.npmmirror.com
+  ;;
+"nju")
+  npm config set registry https://repo.nju.edu.cn/repository/npm/
+  ;;
+*)
+  echo "Invalid mirror option: ${MIRROR}"
+  exit 1
+  ;;
 esac
 
 # 安装 pnpm
